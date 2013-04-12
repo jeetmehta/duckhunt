d5ef33e76dd15806136d80a8ae8f5a3769c0f286
j4mehta <jeet.mehta@kik.com>
2013-04-12 09:29:27 -0400

    added the xcode project, no actual code has been written yet, just added the libraries and such to the project

---
+++ DuckHunt/DuckHunt/main.mm
@@ -0,0 +1,15 @@
+//  main.mm
+//  Duck Hunt
+//  Created by Jeet Mehta on 2013-04-12.
+//  Copyright (c) 2013 Jeet Mehta. All rights reserved.
+
+#include <iostream>
+#include <SDL/SDL.h>
+#include <SDL_ttf/SDL_ttf.h>
+#include <SDL_image/SDL_image.h>
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
+
